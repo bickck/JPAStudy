741f05e1bce253f4030d75ac6b59e9761ce8d48d
bickck <3d1935@naver.com>
2021-11-03 13:39:56 +0900

    2021-11-03

--- src/main/java/com/cos/blog/ListService/ListService.java
+++ src/main/java/com/cos/blog/ListService/ListService.java
@@ -1,18 +1,11 @@
 package com.cos.blog.ListService;
 
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Optional;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 
-//import rg.springframework.transaction.annotation;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,6 +36,7 @@ public class ListService {
 		
 		BoardList boardList = new BoardList("test", "testBody", user.getName());
 		saveBoard(boardList);
+		entityManager.createNativeQuery(null);
 		
 	}
 	
